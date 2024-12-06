2fd76f417e567fe4c7606eb0b04561f16223f96e
shanemalka9 <shanemalka@outlook.com>
2024-12-06 15:36:49 +0530

    made changes

--- src/App.tsx
+++ src/App.tsx
@@ -2,15 +2,12 @@ import IntroPage from "./component/ChatInterface/ChatInterface.tsx";
 import ChatInterface from "./component/ChatInterface/ChatInterface.tsx";
 
 function App() {
-<<<<<<< HEAD
-  return <div><ChatInterface /></div>;
-=======
+
   return (
     <div>
       <IntroPage />
     </div>
   );
->>>>>>> 3d7247f6db672725e230d51a886e99212fee5779
 }
 
 export default App;
