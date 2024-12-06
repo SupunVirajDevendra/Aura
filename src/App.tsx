b58b7d782c5fd99cc8038594b2b77ae73445bce7
shanemalka9 <shanemalka@outlook.com>
2024-12-06 16:35:18 +0530

    IntroPage: finished up file

--- src/App.tsx
+++ src/App.tsx
@@ -2,11 +2,7 @@ import IntroPage from "./component/IntroPage/IntroPage.tsx";
 import ChatInterface from "./component/ChatInterface/ChatInterface.tsx";
 
 function App() {
-<<<<<<< HEAD
   return <div><ChatInterface /></div>;
-=======
-
->>>>>>> 2fd76f417e567fe4c7606eb0b04561f16223f96e
   return (
     <div>
       <IntroPage />
