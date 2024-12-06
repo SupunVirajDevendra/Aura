0b4ba95ab2abf1b31c7d584751c92e4b099baf6e
shanemalka9 <shanemalka@outlook.com>
2024-12-06 13:57:29 +0530

    Added space for images in intro page

--- src/component/IntroPage/IntroPage.tsx
+++ src/component/IntroPage/IntroPage.tsx
@@ -7,7 +7,11 @@ function IntroPage() {
       <img src="" alt="profile image" className={style.profileImage} />
       <div className={style.main_div}>
         <div className={style.intro_div}>
+          <img src="" alt="" />
+          <div className={style.title}>
           <h1>Meet <span>Aura</span></h1>
+          </div>
+          <img src="" alt="" />
         </div>
         <button className={style.start_button}>
         <img src={ufoImage} alt="UFO" />
